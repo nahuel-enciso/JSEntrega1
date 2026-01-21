d6e545148d4df0cf605d08786ff90e15720d648e
Nahuel Enciso <nahuelenciso@gmail.com>
2026-01-21 14:33:17 -0300

    cambio para hacer sync con el repositorio remoto

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Creo un objeto llamado "patentes" que guarda los valores de cada bimestre
 
-const patentes = {
+const patentes = { 
     "Enero-Febrero": undefined,
     "Marzo/Abril": undefined,
     "Mayo/Junio": undefined,
